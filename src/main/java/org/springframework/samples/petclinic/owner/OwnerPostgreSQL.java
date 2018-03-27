f2622ace8e3e7de557a11352478dc2db028b4c07
Corey Levine <coreylevine123@gmail.com>
2018-03-27 16:51:52 -0400

    #9 Add consistency checker. Still need to fix when an inconsistency is detected

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerPostgreSQL.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerPostgreSQL.java
@@ -1,12 +1,15 @@
 package org.springframework.samples.petclinic.owner;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
 
 public class OwnerPostgreSQL {
+	
+	private int inconsistencies = 0;
 	
 	public Connection getConnection(){
 		return ConnectToPostgreSQL.connectToDatabase();
@@ -32,6 +35,81 @@ public class OwnerPostgreSQL {
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	public void consistencyCheck(OwnerRepository repo) throws SQLException {
+
+		List<Owner> owners = repo.getAllOwners();
+
+		String sqlStatement = "SELECT * FROM Owner;";
+		Statement statement = getConnection().createStatement();
+		ResultSet postgresOwners = statement.executeQuery(sqlStatement);
+
+		int idColumn = postgresOwners.findColumn("id");
+		int firstNameColumn = postgresOwners.findColumn("first_name");
+		int lastNameColumn = postgresOwners.findColumn("last_name");
+		int addressColumn = postgresOwners.findColumn("address");
+		int cityColumn = postgresOwners.findColumn("city");
+		int telephoneColumn = postgresOwners.findColumn("telephone");
+
+		int index = 0;
+
+		while(index < owners.size()) {
+
+			int id = 0;
+			String firstName = null;
+			String lastName = null;
+			String address = null;
+			String city = null;
+			String telephone = null;
+
+			if(postgresOwners.next()) {
+				id = (int) postgresOwners.getObject(idColumn);
+				firstName = (String) postgresOwners.getObject(firstNameColumn);
+				lastName = (String) postgresOwners.getObject(lastNameColumn);
+				address = (String) postgresOwners.getObject(addressColumn);
+				city = (String) postgresOwners.getObject(cityColumn);
+				telephone = (String) postgresOwners.getObject(telephoneColumn);
+			}
+
+			Owner owner = owners.get(index);
+			if(owner.getId() != id) {
+				Inconsistency(owner.getId(), id);
+				//fix inconsistency
+			}
+			if(!(owner.getFirstName().equals(firstName))) {
+				Inconsistency(owner.getFirstName(), firstName);
+				//fix inconsistency
+			}
+			if(!(owner.getLastName().equals(lastName))) {
+				Inconsistency(owner.getLastName(), lastName);
+				//fix inconsistency
+			}
+			if(!(owner.getAddress().equals(address))) {
+				Inconsistency(owner.getAddress(), address);
+				//fix inconsistency
+			}
+			if(!(owner.getCity().equals(city))) {
+				Inconsistency(owner.getCity(), city);
+				//fix inconsistency
+			}
+			if(!(owner.getTelephone().equals(telephone))) {
+				Inconsistency(owner.getTelephone(), telephone);
+				//fix inconsistency
+			}
+
+			index++;
+		}
+
+		System.out.println("\nINCONSISTENCIES: " + inconsistencies);
+	}
+	
+	private void Inconsistency(Object expected, Object actual) {
+		System.out.println("\nConsistency Violation!"
+							+ "\n\t Expected: " + expected 
+							+ "\n\t Actual: " + actual);
+		
+		inconsistencies++;
 	}
 
 }
