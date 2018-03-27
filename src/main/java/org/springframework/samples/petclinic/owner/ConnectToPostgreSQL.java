3549a18cde2abbe9e85fb938f3ad2d4666d83330
Corey Levine <coreylevine123@gmail.com>
2018-03-27 16:35:37 -0400

    #8 Create Owners table in new database (if it doesn't already exist)

--- src/main/java/org/springframework/samples/petclinic/owner/ConnectToPostgreSQL.java
+++ src/main/java/org/springframework/samples/petclinic/owner/ConnectToPostgreSQL.java
@@ -9,6 +9,20 @@ public class ConnectToPostgreSQL {
 
 	static Connection connection = null;
 
+	private static void createOwnerTable() throws SQLException {
+
+		String sql_stmt = "CREATE TABLE IF NOT EXISTS Owner (id INTEGER PRIMARY KEY, "
+				+ "first_name VARCHAR(255) NOT NULL, "
+				+ "last_name VARCHAR(255) NOT NULL, "
+				+ "address VARCHAR(255) NOT NULL, "
+				+ "city VARCHAR(255) NOT NULL, "
+				+ "telephone VARCHAR(15) NOT NULL);";
+
+		Statement statement = connection.createStatement();
+		statement.executeUpdate(sql_stmt);
+	}
+
+
 	public static Connection connectToDatabase() {
 		try {
 			Class.forName("org.postgresql.Driver");
@@ -22,6 +36,8 @@ public class ConnectToPostgreSQL {
 			DriverManager.setLoginTimeout(23);
 			connection = DriverManager.getConnection(
 					"jdbc:postgresql://127.0.0.1:5432/petclinic", "postgres", "soen");
+
+			createOwnerTable();
 		} 
 		catch (SQLException e) {
 			System.out.println("Connection Failed! Check output console");
